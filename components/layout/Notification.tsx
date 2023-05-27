e0fa831e4e348ccd8f82edb95c097cf804359c57
Mohammed Hayder <mr.hamood277@gmail.com>
2023-05-27 13:18:41 +0300

    notification css changes

--- components/layout/Notification.tsx
+++ components/layout/Notification.tsx
@@ -1,25 +1,32 @@
 'use client'
-import { useRef } from 'react'
+// import { useRef } from 'react'
 import { notificationProps } from '@/types'
 
 const Notification: React.FC<notificationProps> = ({ children, isOk = false }) => {
-  const notificationRef = useRef<HTMLButtonElement>(null)
+  // const notificationRef = useRef<HTMLButtonElement>(null)
 
-  const removeNotification = () => {
-    notificationRef.current?.parentElement?.remove()
-  }
+  // const removeNotification = () => {
+  //   notificationRef.current?.parentElement?.remove()
+  // }
 
   return (
     <div
-      className={`relative flex justify-center items-center border border-solid my-4 rounded-lg text-center transition-all duration-500 ${
+      className={`relative flex justify-between items-center border border-solid my-4 rounded-lg text-center ${
         isOk
           ? 'bg-green-100 text-green-800 border-green-700'
           : 'bg-red-100 text-red-800 border-red-700'
       }`}
     >
-      <button
+      <p
+        className={`flex-1 p-3 font-bold text-sm sm:text-base mb-0 ${
+          isOk ? 'text-green-800' : 'text-red-800'
+        }`}
+      >
+        {children}
+      </p>
+      {/* <button
         type='button'
-        className={`absolute text-xl font-normal cursor-pointer right-4 hover:font-bold ${
+        className={`text-2xl cursor-pointer hover:font-bold ${
           isOk ? 'bg-green-300 text-green-800' : 'bg-red-300 text-red-800'
         }`}
         aria-label='close noification'
@@ -28,14 +35,7 @@ const Notification: React.FC<notificationProps> = ({ children, isOk = false }) =
         ref={notificationRef}
       >
         &times;
-      </button>
-      <p
-        className={`py-4 px-6 font-bold text-sm sm:text-base ${
-          isOk ? 'text-green-800' : 'text-red-800'
-        }`}
-      >
-        {children}
-      </p>
+      </button> */}
     </div>
   )
 }
