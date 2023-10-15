b3806daf3d980f63aef5f8182faeb71b748f0c0b
Mohammed Hayder <mr.hamood277@gmail.com>
2023-10-15 13:04:48 +0100

    react-scroll-parallax removed

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -9,7 +9,6 @@ import useEventListener from '@/hooks/useEventListener'
 import { SessionProvider } from 'next-auth/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { ParallaxProvider } from 'react-scroll-parallax'
 import { AppProvider } from '@/context/appContext'
 import { ToastContainer } from 'react-toastify'
 
@@ -40,49 +39,47 @@ const Layout = ({
   return (
     <SessionProvider>
       <QueryClientProvider client={queryClientRef.current}>
-        <ParallaxProvider>
-          <AppProvider>
-            <div
-              className={`wrapper relative min-h-screen w-full bg-grey ${
-                blurred ? 'blurredBg' : ''
+        <AppProvider>
+          <div
+            className={`wrapper relative min-h-screen w-full bg-grey ${
+              blurred ? 'blurredBg' : ''
+            }`}
+          >
+            <Header />
+            <main
+              className={`page-content relative bg-grey bg-opacity-95 ${
+                blurred ? 'backdrop-blur-lg backdrop-filter' : ''
               }`}
             >
-              <Header />
-              <main
-                className={`page-content relative bg-grey bg-opacity-95 ${
-                  blurred ? 'backdrop-blur-lg backdrop-filter' : ''
-                }`}
-              >
-                <div className='fixed top-0 left-0 z-20 flex justify-around w-full h-screen bglines'>
-                  <span className='border-r border-white border-opacity-5'></span>
-                  <span className='border-r border-white border-opacity-5'></span>
-                  <span className='border-r border-white border-opacity-5'></span>
-                  <span className='border-r border-white border-opacity-5'></span>
-                  <span className='border-r border-white border-opacity-5'></span>
-                </div>
-                <div className='relative z-30 min-h-screen sitedata'>{children}</div>
-              </main>
-              <Footer />
-              <motion.button
-                initial={{
-                  opacity: 0,
-                  x: 1000
-                }}
-                animate={{
-                  opacity: backToTop ? 1 : 0,
-                  x: backToTop ? 0 : 1000
-                }}
-                className='btn fixed bottom-12 left-auto top-auto right-7 z-30 rounded-full p-2.5 text-xl'
-                aria-label='Scroll To The Top'
-                onClick={() => scroll.scrollToTop()}
-              >
-                <RiArrowUpSLine />
-              </motion.button>
-            </div>
-          </AppProvider>
-          <ToastContainer />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </ParallaxProvider>
+              <div className='fixed top-0 left-0 z-20 flex justify-around w-full h-screen bglines'>
+                <span className='border-r border-white border-opacity-5'></span>
+                <span className='border-r border-white border-opacity-5'></span>
+                <span className='border-r border-white border-opacity-5'></span>
+                <span className='border-r border-white border-opacity-5'></span>
+                <span className='border-r border-white border-opacity-5'></span>
+              </div>
+              <div className='relative z-30 min-h-screen sitedata'>{children}</div>
+            </main>
+            <Footer />
+            <motion.button
+              initial={{
+                opacity: 0,
+                x: 1000
+              }}
+              animate={{
+                opacity: backToTop ? 1 : 0,
+                x: backToTop ? 0 : 1000
+              }}
+              className='btn fixed bottom-12 left-auto top-auto right-7 z-30 rounded-full p-2.5 text-xl'
+              aria-label='Scroll To The Top'
+              onClick={() => scroll.scrollToTop()}
+            >
+              <RiArrowUpSLine />
+            </motion.button>
+          </div>
+        </AppProvider>
+        <ToastContainer />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </SessionProvider>
   )
--- components/layout/Layout2.tsx
+++ components/layout/Layout2.tsx
@@ -8,7 +8,6 @@ import Sidemenu from './Sidemenu'
 import { SessionProvider } from 'next-auth/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { ParallaxProvider } from 'react-scroll-parallax'
 import { AppProvider } from '@/context/appContext'
 import { ToastContainer } from 'react-toastify'
 
@@ -33,80 +32,78 @@ const Layout2 = ({
   return (
     <SessionProvider>
       <QueryClientProvider client={queryClientRef.current}>
-        <ParallaxProvider>
-          <AppProvider>
-            <div
-              className={`wrapper relative min-h-screen w-full bg-grey${
-                blurred ? ' blurredBg' : ''
-              }${whiteTiles ? ' whiteTiles' : ''}`}
+        <AppProvider>
+          <div
+            className={`wrapper relative min-h-screen w-full bg-grey${
+              blurred ? ' blurredBg' : ''
+            }${whiteTiles ? ' whiteTiles' : ''}`}
+          >
+            <button
+              className='mobilemenu-trigger fixed left-0 top-[50vh] z-[60] block h-10 min-h-0 w-8 rounded-bl-none rounded-tl-none border-0 bg-primary bg-opacity-20 p-0 text-3xl text-primary lg:hidden'
+              onClick={() => setFullMobileMenu(true)}
             >
-              <button
-                className='mobilemenu-trigger fixed left-0 top-[50vh] z-[60] block h-10 min-h-0 w-8 rounded-bl-none rounded-tl-none border-0 bg-primary bg-opacity-20 p-0 text-3xl text-primary lg:hidden'
-                onClick={() => setFullMobileMenu(true)}
-              >
-                <RiArrowRightLine />
-              </button>
+              <RiArrowRightLine />
+            </button>
 
-              <Sidemenu
-                fullMenu={fullMenu}
-                fullMenuHandler={setFullMenu}
-                fullMobileMenu={fullMobileMenu}
-                fullMobileMenuHandler={setFullMobileMenu}
-              />
+            <Sidemenu
+              fullMenu={fullMenu}
+              fullMenuHandler={setFullMenu}
+              fullMobileMenu={fullMobileMenu}
+              fullMobileMenuHandler={setFullMobileMenu}
+            />
 
-              <AnimatePresence>
-                {fullMenu && (
-                  <motion.div
-                    initial={{ y: '100vh' }}
-                    animate={{ y: '0px' }}
-                    exit={{ y: '-100vh' }}
-                    transition={{
-                      duration: 0.5
-                    }}
-                    className='fixed top-0 z-40 hidden min-h-screen fullscreen-menu left-20 bg-grey lg:block'
-                  >
-                    <FullscreenMenu fullMenuHandler={setFullMenu} />
-                  </motion.div>
-                )}
-              </AnimatePresence>
+            <AnimatePresence>
+              {fullMenu && (
+                <motion.div
+                  initial={{ y: '100vh' }}
+                  animate={{ y: '0px' }}
+                  exit={{ y: '-100vh' }}
+                  transition={{
+                    duration: 0.5
+                  }}
+                  className='fixed top-0 z-40 hidden min-h-screen fullscreen-menu left-20 bg-grey lg:block'
+                >
+                  <FullscreenMenu fullMenuHandler={setFullMenu} />
+                </motion.div>
+              )}
+            </AnimatePresence>
 
-              <AnimatePresence>
-                {fullMobileMenu && (
-                  <motion.div
-                    initial={{ translateY: '100vh' }}
-                    animate={{ translateY: '0px' }}
-                    exit={{ translateY: '-100vh' }}
-                    transition={{
-                      duration: 0.5
-                    }}
-                    className='fixed left-0 top-0 z-[70] block h-screen w-screen bg-grey-darken lg:hidden'
-                  >
-                    <MobileFullmenu fullMenuHandler={setFullMobileMenu} />
-                  </motion.div>
-                )}
-              </AnimatePresence>
+            <AnimatePresence>
+              {fullMobileMenu && (
+                <motion.div
+                  initial={{ translateY: '100vh' }}
+                  animate={{ translateY: '0px' }}
+                  exit={{ translateY: '-100vh' }}
+                  transition={{
+                    duration: 0.5
+                  }}
+                  className='fixed left-0 top-0 z-[70] block h-screen w-screen bg-grey-darken lg:hidden'
+                >
+                  <MobileFullmenu fullMenuHandler={setFullMobileMenu} />
+                </motion.div>
+              )}
+            </AnimatePresence>
 
-              <main
-                className={`page-content relative ml-0 bg-grey bg-opacity-95 lg:ml-20 ${
-                  blurred ? 'backdrop-blur-lg backdrop-filter' : ''
-                }`}
-              >
-                <div className='fixed top-0 right-0 left-auto z-20 flex justify-around w-full h-screen bglines'>
-                  <span className='border-r border-white border-opacity-5'></span>
-                  <span className='border-r border-white border-opacity-5'></span>
-                  <span className='border-r border-white border-opacity-5'></span>
-                  <span className='border-r border-white border-opacity-5'></span>
-                  <span className='border-r border-white border-opacity-5'></span>
-                </div>
-                <div className='relative z-30 flex items-center justify-center min-h-screen sitedata'>
-                  <div className='w-full'>{children}</div>
-                </div>
-              </main>
-            </div>
-          </AppProvider>
-          <ToastContainer />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </ParallaxProvider>
+            <main
+              className={`page-content relative ml-0 bg-grey bg-opacity-95 lg:ml-20 ${
+                blurred ? 'backdrop-blur-lg backdrop-filter' : ''
+              }`}
+            >
+              <div className='fixed top-0 right-0 left-auto z-20 flex justify-around w-full h-screen bglines'>
+                <span className='border-r border-white border-opacity-5'></span>
+                <span className='border-r border-white border-opacity-5'></span>
+                <span className='border-r border-white border-opacity-5'></span>
+                <span className='border-r border-white border-opacity-5'></span>
+                <span className='border-r border-white border-opacity-5'></span>
+              </div>
+              <div className='relative z-30 flex items-center justify-center min-h-screen sitedata'>
+                <div className='w-full'>{children}</div>
+              </div>
+            </main>
+          </div>
+        </AppProvider>
+        <ToastContainer />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </SessionProvider>
   )
