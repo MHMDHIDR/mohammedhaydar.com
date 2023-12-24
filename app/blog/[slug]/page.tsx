6752d5b410fe75a0204787bcd0f985889a594976
Mohammed Haydar <mr.hamood277@gmail.com>
2023-12-24 20:15:29 +0000

    sanity styled-component is required and added

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -46,7 +46,7 @@ const Blog = async ({ params }: { params: { slug: string } }) => {
 
   const marked = new Marked(
     markedHighlight({
-      langPrefix: 'hljs language-',
+      langPrefix: 'language-',
       highlight(code, lang) {
         const language = hljs.getLanguage(lang) ? lang : 'plaintext'
         return hljs.highlight(code, { language }).value
