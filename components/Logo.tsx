c41f4023daee5a7cf208cd3de31402e5efc5cd86
Mohammed Haydar <mr.hamood277@gmail.com>
2025-01-31 10:14:20 +0000

    Modified Projects and now we have a better structure and added new project as well

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,17 +1,17 @@
-import { cn } from "@/lib/utils"
-import Image from "next/image"
-import Link from "next/link"
-import React from "react"
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
 
 export function Logo({ className }: { className?: string }) {
   return (
-    <Link href={"/"}>
+    <Link href={'/'}>
       <Image
-        src="/logo.svg"
+        src='/logo.svg'
         width={35}
         height={35}
-        alt="Mohammed Ibrahim"
-        className={cn("cursor-pointer", className)}
+        alt='Mohammed Ibrahim'
+        className={cn('cursor-pointer w-8 h-8', className)}
       />
     </Link>
   )
--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,18 @@
-import type { NextConfig } from "next"
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "technodevlabs.s3.eu-west-2.amazonaws.com",
-        protocol: "https",
-        port: ""
+        hostname: 'technodevlabs.s3.eu-west-2.amazonaws.com',
+        protocol: 'https',
+        port: '',
       },
-      {
-        hostname: "s3.eu-west-2.amazonaws.com",
-        protocol: "https",
-        port: ""
-      }
-    ]
+      { hostname: 's3.eu-west-2.amazonaws.com', protocol: 'https', port: '' },
+      { hostname: 'dev.mohammedhaydar.com', protocol: 'https', port: '' },
+    ],
   },
-  experimental: { serverActions: { bodySizeLimit: "10mb" } }
+  experimental: { serverActions: { bodySizeLimit: '10mb' } },
 }
 
 export default nextConfig
