236d60c728615b121cbdf11bf8e7527345b9f8f3
Mohammed Haydar <mr.hamood277@gmail.com>
2025-02-09 11:30:55 +0000

    removed buzz words

--- components/HomeDescription.tsx
+++ components/HomeDescription.tsx
@@ -1,12 +1,12 @@
-"use client"
+'use client'
 
-import { useTypeWriter } from "@/hooks/use-type-writer"
-import { useEffect, useState } from "react"
-import { motion } from "motion/react"
+import { useTypeWriter } from '@/hooks/use-type-writer'
+import { useEffect, useState } from 'react'
+import { motion } from 'motion/react'
 const HomeDescription = () => {
   const [hasLoaded, setHasLoaded] = useState(false)
   const description =
-    "Results-driven Full Stack Developer with 8+ years of experience specializing in React.js, Next.js and modern web technologies. Proven track record of delivering high-performance web applications with a focus on code quality and team collaboration."
+    'Full Stack Developer with 8+ years in React.js, Next.js, and modern web tech, building fast, reliable applications with clean code and strong teamwork.'
   const { displayedText, isComplete } = useTypeWriter(description, 30)
 
   useEffect(() => {
@@ -15,18 +15,18 @@ const HomeDescription = () => {
 
   return (
     <motion.p
-      className="w-auto font-normal leading-7 mb-6 min-h-30"
+      className='w-auto font-normal leading-7 mb-6 min-h-30'
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ delay: 1, duration: 0.5 }}
     >
       {hasLoaded ? (
-        displayedText.split("").map((char, index) => (
+        displayedText.split('').map((char, index) => (
           <motion.span
             key={index}
-            initial={{ color: "rgb(156 163 175)" }}
+            initial={{ color: 'rgb(156 163 175)' }}
             animate={{
-              color: isComplete ? "rgb(255 255 255)" : "rgb(156 163 175)"
+              color: isComplete ? 'rgb(255 255 255)' : 'rgb(156 163 175)',
             }}
             transition={{ duration: 0.5, delay: index * 0.03 }}
           >
