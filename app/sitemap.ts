2c43567200e4e68e4a1e28585beb36ef3c5e1ff2
Mohammed Haydar <mr.hamood277@gmail.com>
2025-03-06 16:43:57 +0000

    Fix sitemap generation and remove extra comma

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,17 +4,17 @@ export const baseUrl = 'https://mohammedhaydar.com'
 
 export default async function sitemap() {
   const { posts } = await getBlogPosts()
-  posts.map(post => ({
+  const postUrls = posts.map(post => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.publishedAt,
   }))
 
-  const routes = ['', '/blog', '/projects', '/resume', , 'services', '/contact'].map(
+  const routes = ['', '/blog', '/projects', '/resume', '/services', '/contact'].map(
     route => ({
       url: `${baseUrl}${route}`,
       lastModified: new Date().toISOString().split('T')[0],
     })
   )
 
-  return [...routes, ...posts]
+  return [...routes, ...postUrls]
 }
