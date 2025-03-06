8a069ac408bcec98a3fe9f30548bebe81367f0be
Mohammed Haydar <mr.hamood277@gmail.com>
2025-03-06 16:35:07 +0000

    Add featured projects carousel to homepage with project details and interactive elements

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,20 @@
-import { getBlogPosts } from "@/app/data-access/posts/get-posts"
+import { getBlogPosts } from '@/app/data-access/posts/get-posts'
 
-export const baseUrl = "https://mohammedhaydar.com"
+export const baseUrl = 'https://mohammedhaydar.com'
 
 export default async function sitemap() {
   const { posts } = await getBlogPosts()
   posts.map(post => ({
     url: `${baseUrl}/blog/${post.slug}`,
-    lastModified: post.publishedAt
+    lastModified: post.publishedAt,
   }))
 
-  const routes = ["", "/blog"].map(route => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split("T")[0]
-  }))
+  const routes = ['', '/blog', '/projects', '/resume', , 'services', '/contact'].map(
+    route => ({
+      url: `${baseUrl}${route}`,
+      lastModified: new Date().toISOString().split('T')[0],
+    })
+  )
 
   return [...routes, ...posts]
 }
