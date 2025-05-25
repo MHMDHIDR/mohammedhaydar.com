81dd3a19fa7c5cab8d6c816826153783908a14f8
Mohammed Haydar <mr.hamood277@gmail.com>
2025-05-25 20:34:02 +0100

    removed unavailable project

--- app/projects/projects.ts
+++ app/projects/projects.ts
@@ -58,17 +58,6 @@ export const projects = [
     githubUrl: 'https://github.com/MHMDHIDR/staymakan',
   },
   {
-    title: 'Restaurant Management System',
-    category: 'Full Stack',
-    description:
-      "Indulge in a delightful experience where finding and ordering your favorite foods, drinks, and sweets has never been easier. Our innovative platform offers a seamless and enjoyable user experience, helping you explore delicious menus from the best restaurants. Whether you're craving local cuisine or international flavors, we've got it all!",
-    stack: ['Next.js', 'TailwindCSS', 'Stripe', 'PayPal', 'MongoDB'],
-    image:
-      'https://technodevlabs.s3.eu-west-2.amazonaws.com/e179af78-e6b8-48f3-84c7-4cf5e1db1e20/4705e6717f3452a4-logo.webp',
-    liveUrl: 'https://restaurant.technodevlabs.com',
-    githubUrl: 'https://github.com/MHMDHIDR/restaurant-app',
-  },
-  {
     title: 'Shms Agricultural',
     category: 'Full Stack',
     description:
